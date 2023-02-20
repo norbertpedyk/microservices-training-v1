e8521786c3563063f99bf153ce4fe670c5737a1d
norbert <norbert.pedyk@gmail.com>
2023-02-20 19:11:04 +0100

    feature: added delete and get endpoints

--- Resource/src/main/java/pl/pedyk/resource/controller/ResourceController.java
+++ Resource/src/main/java/pl/pedyk/resource/controller/ResourceController.java
@@ -1,27 +1,36 @@
 package pl.pedyk.resource.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.pedyk.resource.service.ResourceService;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.Map;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/resources")
 public class ResourceController {
 
     private final ResourceService resourceService;
 
-    @Autowired
-    public ResourceController(ResourceService resourceService) {
-        this.resourceService = resourceService;
+
+    @PostMapping
+    public ResponseEntity<Map<String, Long>> uploadResource(@RequestBody byte[] audioData) {
+        return ResponseEntity.ok().body(resourceService.saveResource(audioData));
     }
 
-    @PostMapping("/resources")
-    public ResponseEntity<Map<String, Long>> uploadResource(@RequestBody byte[] audioData) {
-        Map<String, Long> response = resourceService.saveResource(audioData);
-        return ResponseEntity.ok().body(response);
+    @GetMapping("/{id}")
+    public ResponseEntity<byte[]> getResource(@PathVariable Long id) throws IOException {
+        return ResponseEntity.ok().body(resourceService.getResource(id));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Map<String, long[]>> deleteResources(@RequestParam("id") String ids) {
+        return ResponseEntity.ok().body(resourceService.deleteResources(ids));
     }
 }
--- Resource/src/main/java/pl/pedyk/resource/service/ResourceService.java
+++ Resource/src/main/java/pl/pedyk/resource/service/ResourceService.java
@@ -1,15 +1,20 @@
 package pl.pedyk.resource.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.DeleteObjectsResult;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pl.pedyk.resource.model.ResourceTracking;
 import pl.pedyk.resource.repository.ResourceTrackingRepository;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ResourceService {
@@ -25,7 +30,6 @@ public class ResourceService {
         this.resourceTrackingRepository = resourceTrackingRepository;
     }
 
-    //todo: ustaw baze danych w properties
     public Map<String, Long> saveResource(byte[] audioData) {
         ResourceTracking resourceTracking = resourceTrackingRepository.save(new ResourceTracking(bucketName));
         InputStream inputStream = new ByteArrayInputStream(audioData);
@@ -38,5 +42,27 @@ public class ResourceService {
         };
     }
 
+    public byte[] getResource(Long id) throws IOException {
+        return s3Client.getObject(bucketName, String.valueOf(id)).getObjectContent().readAllBytes();
+    }
+
+    public Map<String, long[]> deleteResources(String ids) {
+        List<String> deletedObjectsIds = s3Client
+                .deleteObjects(new DeleteObjectsRequest(bucketName)
+                        .withKeys(ids))
+                .getDeletedObjects().stream()
+                .map(DeleteObjectsResult.DeletedObject::getKey).toList();
+
+        resourceTrackingRepository.deleteAllById(deletedObjectsIds.stream()
+                .map(Long::parseLong)
+                .collect(Collectors.toList()));
+        return new HashMap<>() {{
+
+            put("id", deletedObjectsIds.stream()
+                    .mapToLong(Long::parseLong)
+                    .toArray());
+        }};
+    }
 }
+
 
