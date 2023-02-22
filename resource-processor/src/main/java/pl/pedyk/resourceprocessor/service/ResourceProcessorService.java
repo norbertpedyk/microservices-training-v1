c297ffeebc0871666e9efe048a9679d435ec1d52
norbert <norbert.pedyk@gmail.com>
2023-02-22 22:58:57 +0100

    feature: logic in resource-processor

---
+++ resource-processor/src/main/java/pl/pedyk/resourceprocessor/service/ResourceProcessorService.java
@@ -0,0 +1,24 @@
+package pl.pedyk.resourceprocessor.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import pl.pedyk.resourceprocessor.model.SongMetadata;
+
+@Service
+public class ResourceProcessorService {
+
+    private final RestTemplate restTemplate;
+
+    public ResourceProcessorService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Value("${custom.song-service-url}")
+    private String songUrl;
+
+    public SongMetadata fetchSongMetadata(String id) {
+        return restTemplate.getForObject(
+                songUrl + id, SongMetadata.class);
+    }
+}
