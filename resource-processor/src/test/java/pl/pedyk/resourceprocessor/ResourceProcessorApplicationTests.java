42611e43e9d60bb792d064746131d6cc70bb90c5
norbert <norbert.pedyk@gmail.com>
2023-02-22 22:25:00 +0100

    feature: started resource-processor service

---
+++ resource-processor/src/test/java/pl/pedyk/resourceprocessor/ResourceProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pedyk.resourceprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
